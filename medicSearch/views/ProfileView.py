a0044f3eeb260e4beac30e2b5e032876172ff8be
DevPovoa <thiagopovoadev@hotmail.com>
2025-07-03 18:44:18 -0300

    adicionado novos templates customizados pela Model

--- medicSearch/views/ProfileView.py
+++ medicSearch/views/ProfileView.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from medicSearch.models.Profile import Profile
 from django.core.paginator import Paginator
+from medicSearch.forms.UserProfileForm import UserProfileForm, UserForm
 
 
 def list_profile_view(request, id=None):
@@ -24,7 +25,6 @@ def list_profile_view(request, id=None):
         page = request.GET.get('page')
         ratings = paginator.get_page(page)
 
-
     context = {
         'profile': profile,
         'favorites': favorites,
@@ -32,3 +32,38 @@ def list_profile_view(request, id=None):
     }
 
     return render(request, template_name='profile/profile.html', context=context, status=200)
+
+
+def edit_profile_view(request):
+    profile = get_object_or_404(Profile, user=request.user)
+    emailUnused = True
+    message = None
+    if request.method == 'POST':
+        profileForm = UserProfileForm(request.POST, request.FILES, instance=profile)
+        userForm = UserForm(request.POST, instance=request.user)
+        verifyEmail = Profile.objects.filter(user__email=request.POST['email']).exclude(
+            user__id=request.user.id).first()
+        emailUnused = verifyEmail is None
+    else:
+        profileForm = UserProfileForm(instance=profile)
+        userForm = UserForm(instance=request.user)
+
+    if profileForm.is_valid() and userForm.is_valid() and emailUnused:
+        profileForm.save()
+        userForm.save()
+        message = {'type': 'success', 'text': 'Dados atualizados com sucesso!'}
+
+    else:
+        if request.method == 'POST':
+            if emailUnused:
+                message = {'type': 'danger', 'text': 'Dados inválidos.'}
+            else:
+                message = {'type': 'warning', 'text': 'E-mail já se encontra em uso por outro usuário.'}
+
+    context = {
+        'profileForm': profileForm,
+        'userForm': userForm,
+        'message': message,
+    }
+
+    return render(request, template_name='user/profile.html', context=context, status=200)
